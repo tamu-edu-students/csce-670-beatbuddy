8fc39918d59cedae30066359450fec07a3a78c62
mpraveen1698 <praveen.kumar.m@tamu.edu>
2024-04-26 13:01:13 -0500

    Recommendations Integration

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -1,6 +1,6 @@
 echo "Deploying Backend..."
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 638316737030.dkr.ecr.us-east-2.amazonaws.com
-docker build -t beatbuddy-backend .
+docker build -t beatbuddy-backend --platform linux/amd64 .  
 docker tag beatbuddy-backend:latest 638316737030.dkr.ecr.us-east-2.amazonaws.com/beatbuddy-backend:latest
 docker push 638316737030.dkr.ecr.us-east-2.amazonaws.com/beatbuddy-backend:latest
 cd aws_deploy
